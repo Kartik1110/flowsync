3554da20a4ca534227b583d324233d2828d5f1cb
Kartik Pal <kartik.p1110@gmail.com>
2024-10-13 15:12:11 +0530

    fix: fixed app page

--- landing-page/app/page.tsx
+++ landing-page/app/page.tsx
@@ -1,21 +1,21 @@
 import React from "react";
 import Navbar from "./components/Navbar";
-// import HeroSection from "./components/HeroSection";
-// import ExamplesSection from "./components/ExamplesSection";
-// import FeaturesSection from "./components/FeaturesSection";
-// import TestimonialsSection from "./components/TestimonialsSection";
-// import GetStartedSection from "./components/GetStartedSection";
+import HeroSection from "./components/HeroSection";
+import ExamplesSection from "./components/ExamplesSection";
+import FeaturesSection from "./components/FeaturesSection";
+import TestimonialsSection from "./components/TestimonialsSection";
+import GetStartedSection from "./components/GetStartedSection";
 import Footer from "./components/Footer";
 
 export default function LandingPage() {
   return (
     <>
       <Navbar />
-      {/* <HeroSection />
+      <HeroSection />
       <ExamplesSection />
       <FeaturesSection />
       <TestimonialsSection />
-      <GetStartedSection /> */}
+      <GetStartedSection />
       <Footer />
     </>
   );
